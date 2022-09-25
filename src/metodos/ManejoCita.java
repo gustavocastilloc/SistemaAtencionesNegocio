9a5a95797e4fbc895f862d28557268784befd532
Gustavo <gusgcast@espol.edu.ec>
2022-09-25 00:08:00 -0500

    cita class

---
+++ src/metodos/ManejoCita.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class ManejoCita {
+    String menucita = "\n\t1.Crear cita\n\t2.Eliminar cita\n\tConsultar cita por fecha";
+    public void crearCita(){
+    
+    }
+}
